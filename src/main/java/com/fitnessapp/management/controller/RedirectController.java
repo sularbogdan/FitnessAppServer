3e0c61de4c9ec608edf22c758368fb13964f7f9b
sularbogdan <sularbogdan@yahoo.com>
2025-07-28 20:43:08 +0300

    redirect to dashboard after payment completion

--- src/main/java/com/fitnessapp/management/controller/RedirectController.java
+++ src/main/java/com/fitnessapp/management/controller/RedirectController.java
@@ -8,12 +8,32 @@ public class RedirectController {
 
     @GetMapping("/success")
     public String success() {
-        return "Your payment was completed successfully!";
+        return "<!DOCTYPE html>" +
+                "<html>" +
+                "<head>" +
+                "<meta http-equiv='refresh' content='5;URL=http://localhost:5173/dashboard-client' />" +
+                "<title>Payment Successful</title>" +
+                "</head>" +
+                "<body>" +
+                "<h2>Your payment was completed successfully!</h2>" +
+                "<p>You will be redirected to your dashboard in 5 seconds...</p>" +
+                "</body>" +
+                "</html>";
     }
 
     @GetMapping("/cancel")
     public String cancel() {
-        return "The payment was canceled.";
+        return  "<!DOCTYPE html>" +
+                "<html>" +
+                "<head>" +
+                "<meta http-equiv='refresh' content='5;URL=http://localhost:5173/dashboard-client' />" +
+                "<title>Payment Canceled</title>" +
+                "</head>" +
+                "<body>" +
+                "<h2>The payment was canceled.</h2>" +
+                "<p>You will be redirected to your dashboard in 5 seconds...</p>" +
+                "</body>" +
+                "</html>";
     }
 }
 
